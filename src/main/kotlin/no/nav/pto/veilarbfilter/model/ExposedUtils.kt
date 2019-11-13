9037a6de01ceb4526da011600dec8fd9d18e4069
alexandraback <alexandra1.back@gmail.com>
2019-11-13 22:16:52 +0100

    FO-2549: Lagra filtervalg som json i databasen. Fjern veilederfilter service

---
+++ src/main/kotlin/no/nav/pto/veilarbfilter/model/ExposedUtils.kt
@@ -0,0 +1,36 @@
+package no.nav.pto.veilarbfilter.model
+
+import no.nav.pto.veilarbfilter.ObjectMapperProvider.Companion.objectMapper
+import org.jetbrains.exposed.sql.Column
+import org.jetbrains.exposed.sql.ColumnType
+import org.jetbrains.exposed.sql.Table
+import org.postgresql.util.PGobject
+import java.sql.PreparedStatement
+
+fun <T : Any> Table.jsonb(name: String, klass: Class<T>): Column<T>
+        = registerColumn(name, Json(klass))
+
+private class Json<out T : Any>(private val klass: Class<T>) : ColumnType() {
+    override fun sqlType() = "jsonb"
+
+    override fun setParameter(stmt: PreparedStatement, index: Int, value: Any?) {
+        val obj = PGobject()
+        obj.type = "jsonb"
+        obj.value = value as String
+        stmt.setObject(index, obj)
+    }
+
+    override fun valueFromDB(value: Any): Any {
+        value as PGobject
+        return try {
+            objectMapper.readValue(value.value, klass)
+        } catch (e: Exception) {
+            e.printStackTrace()
+            throw RuntimeException("Can't parse JSON: $value")
+        }
+    }
+
+
+    override fun notNullValueToDB(value: Any): Any = objectMapper.writeValueAsString(value)
+    override fun nonNullValueToString(value: Any): String = "'${objectMapper.writeValueAsString(value)}'"
+}
