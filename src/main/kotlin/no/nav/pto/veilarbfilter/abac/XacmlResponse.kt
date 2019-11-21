9741b4e8a2eb7ea042b902e6d105ec105a81537d
alexandraback <alexandra1.back@gmail.com>
2019-11-21 10:06:13 +0100

    FO-2549: fiks deserialisering

--- src/main/kotlin/no/nav/pto/veilarbfilter/abac/XacmlResponse.kt
+++ src/main/kotlin/no/nav/pto/veilarbfilter/abac/XacmlResponse.kt
@@ -14,11 +14,24 @@ data class XacmlResponse (
 )
 
 data class Response (
-    @JsonProperty("Decision") val decision: Decision? = null,
-    @JsonProperty("AssociatedAdvice") val associatedAdvice: Advice? = null
+    @JsonProperty("Decision") val decision: Decision?,
+    @JsonProperty("AssociatedAdvice") val associatedAdvice: Advice?,
+    @JsonProperty("Status") val status: Status?
 )
 
 data class Advice (
     @JsonProperty("Id") val id: String? = null,
-    @JsonProperty("AttributeAssignment") val attributeAssignment: List<Attribute>? = null
+    @JsonProperty("AttributeAssignment") val attributeAssignment: List<AttributeAssignment>? = null
+)
+
+data class Status (
+    @JsonProperty("StatusCode") val statusCode: String? = null,
+    @JsonProperty("Value") val value: Any? = null
+)
+
+data class AttributeAssignment (
+    @JsonProperty("AttributeId") val attributeId: String?,
+    @JsonProperty("Value") val value: Any?,
+    @JsonProperty("Category") val category: String?,
+    @JsonProperty("DataType") val dataType: String?
 )
