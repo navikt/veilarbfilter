961f1605b9bf2d112a58f3159fc90d02633085a3
alexandraback <alexandra1.back@gmail.com>
2019-11-29 12:36:09 +0100

    FO-2578: Legg til endepunkter

--- src/main/kotlin/no/nav/pto/veilarbfilter/routes/VeilarfiltreringRoutes.kt
+++ src/main/kotlin/no/nav/pto/veilarbfilter/routes/VeilarfiltreringRoutes.kt
@@ -1,5 +1,6 @@
 package no.nav.pto.veilarbfilter.routes
 
+import io.ktor.application.ApplicationCall
 import no.nav.pto.veilarbfilter.model.NyttFilterModel
 import no.nav.pto.veilarbfilter.service.EnhetFilterService
 import io.ktor.application.call
@@ -8,41 +9,60 @@ import io.ktor.http.HttpStatusCode
 import io.ktor.request.receive
 import io.ktor.response.respond
 import io.ktor.routing.*
+import io.ktor.util.pipeline.PipelineContext
 import no.nav.pto.veilarbfilter.JwtUtil.Companion.getSubject
 import no.nav.pto.veilarbfilter.abac.PepClient
+import no.nav.pto.veilarbfilter.model.FilterModel
 
+suspend fun PipelineContext<Unit, ApplicationCall>.pepAuth(pepClient: PepClient, build: suspend PipelineContext<Unit, ApplicationCall>.(id: String) -> Unit) {
+    val ident = getSubject(call)
+    val enhetId = call.parameters["enhetId"]
+
+    enhetId?.let {
+        if (!it.matches("\\d{4}$".toRegex())) {
+            call.respond(HttpStatusCode.BadRequest)
+        } else if (!pepClient.harTilgangTilEnhet(ident, it)) {
+            call.respond(HttpStatusCode.Forbidden)
+        } else {
+            build.invoke(this, it)
+        }
+    }
+}
 
 fun Route.veilarbfilterRoutes(enhetFilterService: EnhetFilterService, pepClient: PepClient) {
-    route("/api/enhet") {
-        authenticate {
-            post {
-                val ident = getSubject(call)
-                call.request.queryParameters["enhetId"]?.let {
-                    if (!it.matches("\\d{4}$".toRegex())) {
-                        call.respond(HttpStatusCode.BadRequest)
-                    }
-                    if (!pepClient.harTilgangTilEnhet(ident, it)) {
-                        call.respond(HttpStatusCode.Forbidden)
-                    }
+    authenticate {
+        route("/api/enhet") {
+            post("/{enhetId}") {
+                pepAuth(pepClient) {
                     val request = call.receive<NyttFilterModel>()
                     val nyttfilter = enhetFilterService.lagreEnhetFilter(it, request)
                     call.respond(nyttfilter)
                 }
             }
-            get {
-                val ident = getSubject(call)
-                call.request.queryParameters["enhetId"]?.let {
-                    if (!it.matches("\\d{4}$".toRegex())) {
-                        call.respond(HttpStatusCode.BadRequest)
-                    }
-                    if (!pepClient.harTilgangTilEnhet(ident, it)) {
-                        call.respond(HttpStatusCode.Forbidden)
-                    }
+            put("/{enhetId}") {
+                pepAuth(pepClient) {
+                    val request = call.receive<FilterModel>()
+                    val nyttfilter = enhetFilterService.oppdaterEnhetFilter(it, request)
+                    call.respond(nyttfilter)
+                }
+            }
+            get("/{enhetId}") {
+                pepAuth(pepClient) {
                     val filterListe = enhetFilterService.finnFilterForEnhet(it)
                     call.respond(filterListe)
+                }
+            }
+            delete("/{enhetId}/filter/{filterId}") {
+                pepAuth(pepClient) {
+                    call.parameters["filterId"]?.let { filter ->
+                        val slettetFilterId = enhetFilterService.slettFilter(it, filter)
+                        if(slettetFilterId == 0 ) {
+                            call.respond(HttpStatusCode.NotFound)
+                        }
+                        call.respond(HttpStatusCode.NoContent)
+                    }
                 }
             }
         }
     }
 }
-
--- src/main/kotlin/no/nav/pto/veilarbfilter/service/EnhetFilterService.kt
+++ src/main/kotlin/no/nav/pto/veilarbfilter/service/EnhetFilterService.kt
@@ -5,8 +5,9 @@ import no.nav.pto.veilarbfilter.model.FilterModel
 import no.nav.pto.veilarbfilter.model.NyttFilterModel
 
 interface EnhetFilterService {
-    suspend fun lagreEnhetFilter (enhetId: String, nyttFilter: NyttFilterModel) : EnhetFilterModel;
-    suspend fun finnFilterForEnhet (enhetId: String) : List<EnhetFilterModel>;
-    suspend fun oppdaterEnhetFilter (enhetId: String, filterValg: FilterModel) : EnhetFilterModel;
-    suspend fun hentFilter (filterId: Int) : EnhetFilterModel?;
+    suspend fun lagreEnhetFilter (enhetId: String, nyttFilter: NyttFilterModel) : EnhetFilterModel
+    suspend fun finnFilterForEnhet (enhetId: String) : List<EnhetFilterModel>
+    suspend fun oppdaterEnhetFilter (enhetId: String, filterValg: FilterModel) : EnhetFilterModel
+    suspend fun hentFilter (filterId: Int) : EnhetFilterModel?
+    suspend fun slettFilter (enhetId: String, filterId: String): Int
 }
