6f79e962a9682c0df74c3a9e4cf9182f5bcd364f
Peder Korsveien <peder.korsveien@nav.no>
2020-01-07 14:00:48 +0100

    KZ-0 Copypaste i maintest

--- src/test/no/nav/pto/veilarbfilter/MainTest.kt
+++ src/test/no/nav/pto/veilarbfilter/MainTest.kt
@@ -2,16 +2,24 @@ package no.nav.pto.veilarbfilter
 
 import no.nav.common.utils.NaisUtils
 import no.nav.pto.veilarbfilter.config.Configuration
+import java.util.concurrent.TimeUnit
 
 fun main() {
 
-    val mockConfig = Configuration(
+    val configuration = Configuration(
         clustername = "",
         serviceUser = NaisUtils.Credentials("foo", "bar"),
         abac = Configuration.Abac(""),
         veilarbveilederConfig = Configuration.VeilarbveilederConfig("")
-
     )
 
-    main(configuration = mockConfig)
+    val applicationState = ApplicationState()
+    val applicationServer = createHttpServer(applicationState = applicationState, configuration = configuration);
+
+    Runtime.getRuntime().addShutdownHook(Thread {
+        applicationState.initialized = false
+        applicationServer.stop(5, 5, TimeUnit.SECONDS)
+    })
+
+    applicationServer.start(wait = true)
 }
