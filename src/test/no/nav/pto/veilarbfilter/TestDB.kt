af8bb31c69b687ec819f6480d2409246bfa18609
Dragutin Vujovic <dragutin.vujovic@nav.no>
2020-06-24 13:40:52 +0200

    fo-2956: run postregresql in docker image for test cases

---
+++ src/test/no/nav/pto/veilarbfilter/TestDB.kt
@@ -0,0 +1,34 @@
+package no.nav.pto.veilarbfilter
+
+import junit.framework.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+import org.testcontainers.containers.PostgreSQLContainer
+import java.sql.Connection
+import java.sql.DriverManager
+import java.sql.ResultSet
+
+
+class DBTest {
+    private lateinit var postgresqlContainer: PostgreSQLContainer<Nothing>;
+
+    @Before
+    fun setUp() {
+        postgresqlContainer = PostgreSQLContainer<Nothing>("postgres:12-alpine")
+        postgresqlContainer.start()
+    }
+    
+    @Test
+    fun someTestMethod() {
+        val url: String = postgresqlContainer.getJdbcUrl()
+        var username: String = postgresqlContainer.getUsername()
+        val password: String = postgresqlContainer.getPassword()
+        val conn: Connection = DriverManager
+            .getConnection(url, username, password)
+        val resultSet: ResultSet = conn.createStatement().executeQuery("SELECT 1")
+        resultSet.next()
+        val result = resultSet.getInt(1)
+
+        assertEquals(1, result)
+    }
+}
