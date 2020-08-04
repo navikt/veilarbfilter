268524c90b0f2c62aa292f5b67eda75706a75a6a
Dragutin Vujovic <dragutin.vujovic@nav.no>
2020-08-04 12:59:11 +0200

    fo-2974: Sync aktiviteter med front-end

--- src/main/no/nav/pto/veilarbfilter/model/PortefoljeFilterModel.kt
+++ src/main/no/nav/pto/veilarbfilter/model/PortefoljeFilterModel.kt
@@ -5,58 +5,60 @@ import com.fasterxml.jackson.annotation.JsonProperty
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 data class PortefoljeFilter(
-    val aktiviteter: Aktiviteter? = null,
-    val alder: List<String> = emptyList(),
-    val ferdigfilterListe: List<String> = emptyList(),
-    val fodselsdagIMnd: List<String> = emptyList(),
-    val formidlingsgruppe: List<String> = emptyList(),
-    val hovedmal: List<String> = emptyList(),
-    val innsatsgruppe: List<String> = emptyList(),
-    val kjonn: String? = "",
-    val manuellBrukerStatus: List<String> = emptyList(),
-    val navnEllerFnrQuery: String = "",
-    val rettighetsgruppe: List<String> = emptyList(),
-    val servicegruppe: List<String> = emptyList(),
-    val tiltakstyper: List<String> = emptyList(),
-    val veilederNavnQuery: String = "",
-    val veiledere: List<String> = emptyList(),
-    val ytelse: String? = "",
-    val registreringstype: List<String>? = emptyList(),
-    val cvJobbprofil: String? = ""
+        val aktiviteter: Aktiviteter? = null,
+        val alder: List<String> = emptyList(),
+        val ferdigfilterListe: List<String> = emptyList(),
+        val fodselsdagIMnd: List<String> = emptyList(),
+        val formidlingsgruppe: List<String> = emptyList(),
+        val hovedmal: List<String> = emptyList(),
+        val innsatsgruppe: List<String> = emptyList(),
+        val kjonn: String? = "",
+        val manuellBrukerStatus: List<String> = emptyList(),
+        val navnEllerFnrQuery: String = "",
+        val rettighetsgruppe: List<String> = emptyList(),
+        val servicegruppe: List<String> = emptyList(),
+        val tiltakstyper: List<String> = emptyList(),
+        val veilederNavnQuery: String = "",
+        val veiledere: List<String> = emptyList(),
+        val ytelse: String? = "",
+        val registreringstype: List<String>? = emptyList(),
+        val cvJobbprofil: String? = ""
 ) {
 
     fun isNotEmpty(): Boolean {
         return listOf(
-            kjonn,
-            navnEllerFnrQuery,
-            veilederNavnQuery,
-            ytelse,
-            cvJobbprofil
+                kjonn,
+                navnEllerFnrQuery,
+                veilederNavnQuery,
+                ytelse,
+                cvJobbprofil
         ).any { it != null && it.isNotEmpty() }
                 || listOf(
-            alder,
-            ferdigfilterListe,
-            fodselsdagIMnd,
-            formidlingsgruppe,
-            hovedmal,
-            innsatsgruppe,
-            manuellBrukerStatus,
-            rettighetsgruppe,
-            servicegruppe,
-            tiltakstyper,
-            veiledere,
-            registreringstype
+                alder,
+                ferdigfilterListe,
+                fodselsdagIMnd,
+                formidlingsgruppe,
+                hovedmal,
+                innsatsgruppe,
+                manuellBrukerStatus,
+                rettighetsgruppe,
+                servicegruppe,
+                tiltakstyper,
+                veiledere,
+                registreringstype
         ).any { it != null && it.isNotEmpty() }
                 || aktiviteter != null
     }
 }
 
 data class Aktiviteter(
-    @get:JsonProperty("BEHANDLING") val BEHANDLING: String?,
-    @get:JsonProperty("EGEN") val EGEN: String?,
-    @get:JsonProperty("GRUPPEAKTIVITET") val GRUPPEAKTIVITET: String?,
-    @get:JsonProperty("IJOBB") val IJOBB: String?,
-    @get:JsonProperty("MOTE") val MOTE: String?,
-    @get:JsonProperty("STILLING") val STILLING: String?,
-    @get:JsonProperty("UTDANNINGAKTIVITET") val UTDANNINGAKTIVITET: String?
+        @get:JsonProperty("BEHANDLING") val BEHANDLING: String?,
+        @get:JsonProperty("EGEN") val EGEN: String?,
+        @get:JsonProperty("GRUPPEAKTIVITET") val GRUPPEAKTIVITET: String?,
+        @get:JsonProperty("IJOBB") val IJOBB: String?,
+        @get:JsonProperty("MOTE") val MOTE: String?,
+        @get:JsonProperty("SOKEAVTALE") val SOKEAVTALE: String?,
+        @get:JsonProperty("STILLING") val STILLING: String?,
+        @get:JsonProperty("TILTAK") val TILTAK: String?,
+        @get:JsonProperty("UTDANNINGAKTIVITET") val UTDANNINGAKTIVITET: String?
 )
