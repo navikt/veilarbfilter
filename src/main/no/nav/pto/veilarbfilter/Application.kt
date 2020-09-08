cd64b3a4cc0d84a187e6a54d78fcddf654d77de6
Dragutin Vujovic <dragutin.vujovic@nav.no>
2020-09-08 13:16:09 +0200

    Remove cleanup from backend since its not working

--- src/main/no/nav/pto/veilarbfilter/Application.kt
+++ src/main/no/nav/pto/veilarbfilter/Application.kt
@@ -5,17 +5,12 @@ import no.nav.common.utils.SslUtils
 import no.nav.pto.veilarbfilter.client.VeilarbveilederClient
 import no.nav.pto.veilarbfilter.config.Configuration
 import no.nav.pto.veilarbfilter.config.Database
-import no.nav.pto.veilarbfilter.jobs.CleanupVeilederGrupper
 import no.nav.pto.veilarbfilter.jobs.MetricsReporter
 import no.nav.pto.veilarbfilter.service.MineLagredeFilterServiceImpl
 import no.nav.pto.veilarbfilter.service.VeilederGrupperServiceImpl
 import java.util.concurrent.TimeUnit
 
 data class ApplicationState(var running: Boolean = true, var initialized: Boolean = false)
-
-
-private val INITIAL_DELAY_CLEANUP = TimeUnit.MINUTES.toMillis(1);
-private val INTERVAL_CLEANUP = TimeUnit.MINUTES.toMillis(15);
 
 private val INITIAL_DELAY_METRICS = TimeUnit.MINUTES.toMillis(2);
 private val INTERVAL_METRICS_REPORT = TimeUnit.MINUTES.toMillis(5);
@@ -62,13 +57,12 @@ fun main(configuration: Configuration) {
 
     Runtime.getRuntime().addShutdownHook(Thread {
         applicationState.initialized = false
-        applicationServer.stop(5, 5)
-        //cleanUpVeilederGrupper.stop()
+        cleanUpVeilederGrupper.stop()
         metrikker.stop()
         
     })
 
-    //cleanUpVeilederGrupper.start()
+    cleanUpVeilederGrupper.start()
     metrikker.start()
     applicationServer.start(wait = configuration.httpServerWait)
 
