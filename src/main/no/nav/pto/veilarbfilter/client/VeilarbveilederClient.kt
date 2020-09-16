c8af0c921be690bb91336150362ebe064c27a4fc
Dragutin Vujovic <dragutin.vujovic@nav.no>
2020-09-16 15:40:12 +0200

    Trigger redeploy

--- src/main/no/nav/pto/veilarbfilter/client/VeilarbveilederClient.kt
+++ src/main/no/nav/pto/veilarbfilter/client/VeilarbveilederClient.kt
@@ -9,17 +9,15 @@ import kotlinx.coroutines.runBlocking
 import no.nav.common.sts.NaisSystemUserTokenProvider
 import no.nav.common.utils.IdUtils
 import no.nav.pto.veilarbfilter.ObjectMapperProvider
-import no.nav.pto.veilarbfilter.client.dto.VeiledereResponse
 import no.nav.pto.veilarbfilter.config.Configuration
-import org.slf4j.LoggerFactory
 
 
 private val veilederPaEnhetenCache = VeilederCache()
 
+
 class VeilarbveilederClient(config: Configuration, systemUserTokenProvider: NaisSystemUserTokenProvider?) {
     private val veilarbveilederClientUrl = config.veilarbveilederConfig.url
     private val systemUserTokenProvider = systemUserTokenProvider;
-    private val log = LoggerFactory.getLogger("VeilarbveilederClient")
 
     fun hentVeilederePaEnheten(enhetId: String): List<String>? {
         val veilederCacheValue = veilederPaEnhetenCache.veilederePaEnheten(enhetId)
@@ -44,25 +42,21 @@ class VeilarbveilederClient(config: Configuration, systemUserTokenProvider: Nais
         enhetId: String
     ): List<String>? {
         val res = response.readText()
-        val veiledereResponse: VeiledereResponse =
-            ObjectMapperProvider.objectMapper.readValue(res, object : TypeReference<VeiledereResponse>() {});
-        val veilederIdentList = veiledereResponse.veilederListe.map { it.ident }
-        veilederPaEnhetenCache.leggTilEnhetICachen(enhetId, veilederIdentList)
-        return veilederIdentList
+        val veiledereResponse: List<String> =
+            ObjectMapperProvider.objectMapper.readValue(res, object : TypeReference<List<String>>() {});
+        veilederPaEnhetenCache.leggTilEnhetICachen(enhetId, veiledereResponse)
+        return veiledereResponse
     }
 
     private suspend fun get(
         httpClient: HttpClient,
         enhetId: String
     ): HttpResponse {
-        return httpClient.get<HttpStatement>("$veilarbveilederClientUrl/api/enhet/$enhetId/veiledere") {
+        return httpClient.get<HttpStatement>("$veilarbveilederClientUrl/api/enhet/$enhetId/identer") {
             header("Nav-Call-Id", IdUtils.generateId())
             header("Nav-Consumer-Id", "veilarbfilter")
             if (systemUserTokenProvider != null) {
-                log.info("Adding user token, token length: " + systemUserTokenProvider.systemUserToken.length)
                 header("Authorization", "Bearer " + systemUserTokenProvider.systemUserToken)
-            } else {
-                log.warn("System user token er null")
             }
         }
             .execute()
