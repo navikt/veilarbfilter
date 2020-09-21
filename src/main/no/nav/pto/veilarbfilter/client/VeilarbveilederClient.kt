75cf0b905f8121dda2c5a2b4a77e76fb5ef96f39
Dragutin Vujovic <dragutin.vujovic@nav.no>
2020-09-21 08:52:40 +0200

    Trigger redeploy

--- src/main/no/nav/pto/veilarbfilter/client/VeilarbveilederClient.kt
+++ src/main/no/nav/pto/veilarbfilter/client/VeilarbveilederClient.kt
@@ -52,6 +52,7 @@ class VeilarbveilederClient(config: Configuration, systemUserTokenProvider: Nais
         httpClient: HttpClient,
         enhetId: String
     ): HttpResponse {
+        
         return httpClient.get<HttpStatement>("$veilarbveilederClientUrl/api/enhet/$enhetId/identer") {
             header("Nav-Call-Id", IdUtils.generateId())
             header("Nav-Consumer-Id", "veilarbfilter")
