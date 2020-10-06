88373edf77e4e7a758dff7271de7452b08e49bcd
Dragutin Vujovic <dragutin.vujovic@nav.no>
2020-10-06 20:33:26 +0200

    Mark in db when filter is cleaned up by cleanup job

--- src/main/no/nav/pto/veilarbfilter/Application.kt
+++ src/main/no/nav/pto/veilarbfilter/Application.kt
@@ -5,12 +5,17 @@ import no.nav.common.utils.SslUtils
 import no.nav.pto.veilarbfilter.client.VeilarbveilederClient
 import no.nav.pto.veilarbfilter.config.Configuration
 import no.nav.pto.veilarbfilter.config.Database
+import no.nav.pto.veilarbfilter.jobs.CleanupVeilederGrupper
 import no.nav.pto.veilarbfilter.jobs.MetricsReporter
 import no.nav.pto.veilarbfilter.service.MineLagredeFilterServiceImpl
 import no.nav.pto.veilarbfilter.service.VeilederGrupperServiceImpl
 import java.util.concurrent.TimeUnit
 
 data class ApplicationState(var running: Boolean = true, var initialized: Boolean = false)
+
+
+private val INITIAL_DELAY_CLEANUP = TimeUnit.MINUTES.toMillis(1);
+private val INTERVAL_CLEANUP = TimeUnit.MINUTES.toMillis(15);
 
 private val INITIAL_DELAY_METRICS = TimeUnit.MINUTES.toMillis(2);
 private val INTERVAL_METRICS_REPORT = TimeUnit.MINUTES.toMillis(5);
@@ -59,7 +64,7 @@ fun main(configuration: Configuration) {
         applicationState.initialized = false
         cleanUpVeilederGrupper.stop()
         metrikker.stop()
-        
+
     })
 
     cleanUpVeilederGrupper.start()
