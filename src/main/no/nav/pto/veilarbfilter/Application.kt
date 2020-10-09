fa97b0dc84e4d84b1cb5bccbdf55596d7344f969
Dragutin Vujovic <dragutin.vujovic@nav.no>
2020-10-09 10:49:15 +0200

    Decrease cleanup time

--- src/main/no/nav/pto/veilarbfilter/Application.kt
+++ src/main/no/nav/pto/veilarbfilter/Application.kt
@@ -15,7 +15,7 @@ data class ApplicationState(var running: Boolean = true, var initialized: Boolea
 
 
 private val INITIAL_DELAY_CLEANUP = TimeUnit.MINUTES.toMillis(1);
-private val INTERVAL_CLEANUP = TimeUnit.MINUTES.toMillis(15);
+private val INTERVAL_CLEANUP = TimeUnit.MINUTES.toMillis(5);
 
 private val INITIAL_DELAY_METRICS = TimeUnit.MINUTES.toMillis(2);
 private val INTERVAL_METRICS_REPORT = TimeUnit.MINUTES.toMillis(5);
