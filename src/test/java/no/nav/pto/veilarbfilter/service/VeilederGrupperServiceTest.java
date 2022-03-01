4c1198c6f1a4fedbfeaf6839b9839ae5529b074b
Dragutin Vujovic <dragutin.vujovic@nav.no>
2022-03-01 17:13:24 +0100

    Fix tests

--- src/test/java/no/nav/pto/veilarbfilter/service/VeilederGrupperServiceTest.java
+++ src/test/java/no/nav/pto/veilarbfilter/service/VeilederGrupperServiceTest.java
@@ -12,12 +12,14 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.Random;
 
 @SpringBootTest
+@ActiveProfiles({"Test"})
 class VeilederGrupperServiceTest extends AbstractTest {
     @Autowired
     private VeilederGrupperService veilederGrupperService;
