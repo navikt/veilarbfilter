01dca687f637be22b3967992e7b230fa196b517b
Dragutin Vujovic <dragutin.vujovic@nav.no>
2022-03-04 13:31:44 +0100

    Bug fixing

--- src/main/java/no/nav/pto/veilarbfilter/config/ApplicationConfig.java
+++ src/main/java/no/nav/pto/veilarbfilter/config/ApplicationConfig.java
@@ -12,16 +12,8 @@ import no.nav.common.sts.NaisSystemUserTokenProvider;
 import no.nav.common.sts.SystemUserTokenProvider;
 import no.nav.common.utils.Credentials;
 import no.nav.pto.veilarbfilter.auth.ModiaPep;
-import no.nav.pto.veilarbfilter.client.VeilarbveilederClient;
 import no.nav.pto.veilarbfilter.domene.deserializer.DateDeserializer;
 import no.nav.pto.veilarbfilter.domene.deserializer.DateSerializer;
-import no.nav.pto.veilarbfilter.repository.MineLagredeFilterRepository;
-import no.nav.pto.veilarbfilter.repository.VeilederGruppeFilterRepository;
-import no.nav.pto.veilarbfilter.rest.MineLagredeFilter;
-import no.nav.pto.veilarbfilter.rest.RestResponseEntityExceptionHandler;
-import no.nav.pto.veilarbfilter.rest.VeilederGruppe;
-import no.nav.pto.veilarbfilter.service.MineLagredeFilterService;
-import no.nav.pto.veilarbfilter.service.VeilederGrupperService;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -76,40 +68,5 @@ public class ApplicationConfig {
         module.addSerializer(LocalDateTime.class, new DateSerializer());
         mapper.registerModule(module);
         return mapper;
-    }
-
-    @Bean
-    public VeilederGruppeFilterRepository veilederGruppeFilterRepository(JdbcTemplate db, MineLagredeFilterRepository mineLagredeFilterRepository, ObjectMapper objectMapper) {
-        return new VeilederGruppeFilterRepository(db, mineLagredeFilterRepository, objectMapper);
-    }
-
-    @Bean
-    public VeilederGrupperService veilederGrupperService(VeilederGruppeFilterRepository veilederGruppeFilterRepository, VeilarbveilederClient veilarbveilederClient) {
-        return new VeilederGrupperService(veilederGruppeFilterRepository, veilarbveilederClient);
-    }
-
-    @Bean
-    public MineLagredeFilterRepository mineLagredeFilterRepository(JdbcTemplate db, ObjectMapper objectMapper) {
-        return new MineLagredeFilterRepository(db, objectMapper);
-    }
-
-    @Bean
-    public MineLagredeFilterService mineLagredeFilterService(MineLagredeFilterRepository mineLagredeFilterRepository) {
-        return new MineLagredeFilterService(mineLagredeFilterRepository);
-    }
-
-    @Bean
-    public RestResponseEntityExceptionHandler restResponseEntityExceptionHandler() {
-        return new RestResponseEntityExceptionHandler();
-    }
-
-    @Bean
-    public MineLagredeFilter mineLagredeFilter(MineLagredeFilterService mineLagredeFilterService) {
-        return new MineLagredeFilter(mineLagredeFilterService);
-    }
-
-    @Bean
-    public VeilederGruppe veilederGruppe(VeilederGrupperService veilederGrupperService) {
-        return new VeilederGruppe(veilederGrupperService);
     }
 }
