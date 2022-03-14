23d1c31009ca9d4c74b48cb057e6fa52209b8d67
Dragutin Vujovic <dragutin.vujovic@nav.no>
2022-03-14 13:45:32 +0100

    Bugfix

--- src/main/java/no/nav/pto/veilarbfilter/client/VeilarbveilederClient.java
+++ src/main/java/no/nav/pto/veilarbfilter/client/VeilarbveilederClient.java
@@ -6,6 +6,7 @@ import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import no.nav.common.rest.client.RestClient;
 import no.nav.common.rest.client.RestUtils;
+import no.nav.common.sts.SystemUserTokenProvider;
 import no.nav.common.types.identer.EnhetId;
 import no.nav.pto.veilarbfilter.config.EnvironmentProperties;
 import okhttp3.OkHttpClient;
@@ -18,20 +19,21 @@ import java.util.concurrent.TimeUnit;
 
 import static java.lang.String.format;
 import static no.nav.common.client.utils.CacheUtils.tryCacheFirst;
-import static no.nav.pto.veilarbfilter.client.RestClientUtils.authHeaderMedSystemBruker;
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 
 @Slf4j
 @Service
-public class VeilarbveilederClient {
 
+public class VeilarbveilederClient {
+    private SystemUserTokenProvider systemUserTokenProvider;
     private String url;
     private OkHttpClient client;
     private Cache<EnhetId, List<String>> hentVeilederePaaEnhetCache;
 
-    public VeilarbveilederClient(EnvironmentProperties environmentProperties) {
+    public VeilarbveilederClient(EnvironmentProperties environmentProperties, SystemUserTokenProvider systemUserTokenProvider) {
         this.url = environmentProperties.getVeilarbVeilederUrl();
         this.client = RestClient.baseClient();
+        this.systemUserTokenProvider = systemUserTokenProvider;
         hentVeilederePaaEnhetCache = Caffeine.newBuilder()
                 .expireAfterWrite(5, TimeUnit.MINUTES)
                 .maximumSize(600)
@@ -48,7 +50,7 @@ public class VeilarbveilederClient {
         String path = format("/enhet/%s/identer", enhet);
 
         Request request = new Request.Builder()
-                .header(AUTHORIZATION, authHeaderMedSystemBruker())
+                .header(AUTHORIZATION, "Bearer " + systemUserTokenProvider.getSystemUserToken())
                 .url(url + path)
                 .build();
 
