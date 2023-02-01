6e6a9125d99a7477477bc3c03065494b40350254
Klara M Helgemo <klara.margrethe.helgemo@nav.no>
2023-02-01 15:55:47 +0100

    Ta i bruk poao-tilgang. Oppdateringer etter mal fra veilarbperson

--- src/main/java/no/nav/pto/veilarbfilter/config/ApplicationConfig.java
+++ src/main/java/no/nav/pto/veilarbfilter/config/ApplicationConfig.java
@@ -2,11 +2,12 @@ package no.nav.pto.veilarbfilter.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
 import net.javacrumbs.shedlock.core.LockProvider;
 import net.javacrumbs.shedlock.provider.jdbctemplate.JdbcTemplateLockProvider;
 import no.nav.common.abac.Pep;
 import no.nav.common.abac.VeilarbPepFactory;
-import no.nav.common.abac.audit.SpringAuditRequestInfoSupplier;
 import no.nav.common.auth.context.AuthContextHolder;
 import no.nav.common.auth.context.AuthContextHolderThreadLocal;
 import no.nav.common.job.leader_election.LeaderElectionClient;
@@ -17,9 +18,7 @@ import no.nav.common.rest.client.RestClient;
 import no.nav.common.token_client.builder.AzureAdTokenClientBuilder;
 import no.nav.common.token_client.client.AzureAdMachineToMachineTokenClient;
 import no.nav.common.utils.Credentials;
-import no.nav.poao_tilgang.client.PoaoTilgangCachedClient;
-import no.nav.poao_tilgang.client.PoaoTilgangClient;
-import no.nav.poao_tilgang.client.PoaoTilgangHttpClient;
+import no.nav.poao_tilgang.client.*;
 import no.nav.pto.veilarbfilter.domene.deserializer.DateDeserializer;
 import no.nav.pto.veilarbfilter.domene.deserializer.DateSerializer;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -29,7 +28,10 @@ import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.core.JdbcTemplate;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
 
 import static no.nav.common.utils.NaisUtils.getCredentials;
 
@@ -38,7 +40,15 @@ import static no.nav.common.utils.NaisUtils.getCredentials;
 @Import(DbConfigPostgres.class)
 @EnableConfigurationProperties({EnvironmentProperties.class})
 public class ApplicationConfig {
-
+    private final Cache<PolicyInput, Decision> policyInputToDecisionCache = Caffeine.newBuilder()
+            .expireAfterWrite(Duration.ofMinutes(30))
+            .build();
+    private final Cache<UUID, List<AdGruppe>> navAnsattIdToAzureAdGrupperCache = Caffeine.newBuilder()
+            .expireAfterWrite(Duration.ofMinutes(30))
+            .build();
+    private final Cache<String, Boolean> norskIdentToErSkjermetCache = Caffeine.newBuilder()
+            .expireAfterWrite(Duration.ofMinutes(30))
+            .build();
     @Bean
     public LockProvider lockProvider(JdbcTemplate jdbcTemplate) {
         return new JdbcTemplateLockProvider(jdbcTemplate);
@@ -54,7 +64,7 @@ public class ApplicationConfig {
         Credentials serviceUserCredentials = getCredentials("service_user");
         return VeilarbPepFactory.get(
                 properties.getAbacUrl(), serviceUserCredentials.username,
-                serviceUserCredentials.password, new SpringAuditRequestInfoSupplier()
+                serviceUserCredentials.password, () -> null
         );
     }
 
@@ -94,9 +104,9 @@ public class ApplicationConfig {
                         () -> tokenClient.createMachineToMachineToken(properties.getPoaoTilgangScope()),
                         RestClient.baseClient()
                 ),
-                null,
-                null,
-                null
+                policyInputToDecisionCache,
+                navAnsattIdToAzureAdGrupperCache,
+                norskIdentToErSkjermetCache
         );
     }
 }
