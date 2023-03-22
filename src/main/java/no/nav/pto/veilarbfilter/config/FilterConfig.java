f07fe7c6203e94856ca2a2bc89a83f78e58a5524
Sondre Larsen Ovrid <sondre.larsen.ovrid@nav.no>
2023-03-22 10:38:07 +0100

    Revert "Merge branch 'main' into dev"
    This reverts commit 399c3817fd6fa41ec9fc13934ed3fc1b4bd36875, reversing
    changes made to ee88d085ba514c280a3850b985a55bde335c89a1.

--- src/main/java/no/nav/pto/veilarbfilter/config/FilterConfig.java
+++ src/main/java/no/nav/pto/veilarbfilter/config/FilterConfig.java
@@ -1,29 +1,29 @@
 package no.nav.pto.veilarbfilter.config;
 
 import no.nav.common.auth.oidc.filter.*;
-import no.nav.common.rest.filter.JavaxLogRequestFilter;
-import no.nav.common.rest.filter.JavaxSetStandardHttpHeadersFilter;
+import no.nav.common.rest.filter.LogRequestFilter;
+import no.nav.common.rest.filter.SetStandardHttpHeadersFilter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import static no.nav.common.auth.oidc.filter.JavaxOidcAuthenticator.fromConfigs;
+import static no.nav.common.auth.oidc.filter.OidcAuthenticator.fromConfigs;
 import static no.nav.common.utils.EnvironmentUtils.isDevelopment;
 import static no.nav.common.utils.EnvironmentUtils.requireApplicationName;
 
 @Configuration
 public class FilterConfig {
-    public JavaxOidcAuthenticatorConfig azureAdAuthConfig(EnvironmentProperties properties) {
-        return new JavaxOidcAuthenticatorConfig()
+    public OidcAuthenticatorConfig azureAdAuthConfig(EnvironmentProperties properties) {
+        return new OidcAuthenticatorConfig()
                 .withDiscoveryUrl(properties.getNaisAadDiscoveryUrl())
                 .withClientId(properties.getNaisAadClientId())
                 .withUserRoleResolver(new AzureAdUserRoleResolver());
     }
 
     @Bean
-    public FilterRegistrationBean<JavaxOidcAuthenticationFilter> authenticationFilterRegistrationBean(EnvironmentProperties properties) {
-        FilterRegistrationBean<JavaxOidcAuthenticationFilter> registration = new FilterRegistrationBean<>();
-        JavaxOidcAuthenticationFilter authenticationFilter = new JavaxOidcAuthenticationFilter(
+    public FilterRegistrationBean<OidcAuthenticationFilter> authenticationFilterRegistrationBean(EnvironmentProperties properties) {
+        FilterRegistrationBean<OidcAuthenticationFilter> registration = new FilterRegistrationBean<>();
+        OidcAuthenticationFilter authenticationFilter = new OidcAuthenticationFilter(
                 fromConfigs(
                         azureAdAuthConfig(properties)
                 )
@@ -36,18 +36,18 @@ public class FilterConfig {
 
 
     @Bean
-    public FilterRegistrationBean<JavaxLogRequestFilter>  logFilterRegistrationBean() {
-        FilterRegistrationBean<JavaxLogRequestFilter> registration = new FilterRegistrationBean<>();
-        registration.setFilter(new JavaxLogRequestFilter(requireApplicationName(), isDevelopment().orElse(false)));
+    public FilterRegistrationBean<LogRequestFilter>  logFilterRegistrationBean() {
+        FilterRegistrationBean<LogRequestFilter> registration = new FilterRegistrationBean<>();
+        registration.setFilter(new LogRequestFilter(requireApplicationName(), isDevelopment().orElse(false)));
         registration.setOrder(2);
         registration.addUrlPatterns("/*");
         return registration;
     }
 
     @Bean
-    public FilterRegistrationBean<JavaxSetStandardHttpHeadersFilter> setStandardHeadersFilterRegistrationBean() {
-        FilterRegistrationBean<JavaxSetStandardHttpHeadersFilter> registration = new FilterRegistrationBean<>();
-        registration.setFilter(new JavaxSetStandardHttpHeadersFilter());
+    public FilterRegistrationBean<SetStandardHttpHeadersFilter> setStandardHeadersFilterRegistrationBean() {
+        FilterRegistrationBean<SetStandardHttpHeadersFilter> registration = new FilterRegistrationBean<>();
+        registration.setFilter(new SetStandardHttpHeadersFilter());
         registration.setOrder(3);
         registration.addUrlPatterns("/*");
         return registration;
