cf56e4bf0d0ea65a1b9479571f85700e40ff3911
Kristine Schuessler <kristine.lynne.schuessler@nav.no>
2023-07-19 13:25:49 +0200

    lagt til tester for slettVisningOverblikk

--- src/test/java/no/nav/pto/veilarbfilter/rest/OverblikkFilterTest.java
+++ src/test/java/no/nav/pto/veilarbfilter/rest/OverblikkFilterTest.java
@@ -1,6 +1,7 @@
 package no.nav.pto.veilarbfilter.rest;
 
 import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.val;
 import no.nav.pto.veilarbfilter.AbstractTest;
 import no.nav.pto.veilarbfilter.domene.ChipsModel;
@@ -14,6 +15,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
@@ -21,11 +23,13 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
+import static org.springframework.http.RequestEntity.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -45,9 +49,49 @@ public class OverblikkFilterTest extends AbstractTest {
     public void beforeEach() {
     }
 
+    @MockBean
+    private ChipsService chipsService;
+
     @Test
     public void testInit() {
         Assertions.assertNotNull(mockMvc);
+    }
+
+    @Test
+    public void testSlettVisning() throws Exception {
+        String veilederId = "someVeilederId";
+        doNothing().when(chipsService).slettVisning(eq(veilederId));
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.delete("/api/overblikkvisning"))
+                .andReturn();
+        Assertions.assertEquals(HttpStatus.OK.value(), mvcResult.getResponse().getStatus());
+    }
+
+    //Hjelpefunskjoner
+    private String slettVisningTest(String veilederId) {
+        try {
+            MvcResult mvcResult = this.mockMvc.perform(MockMvcRequestBuilders.delete("/api/overblikkvisning/" + veilederId).accept(MediaType.APPLICATION_JSON).contentType(MediaType.APPLICATION_JSON)).andReturn();
+            return mvcResult.getResponse().getContentAsString();
+        } catch (Exception e) {
+            Assertions.fail();
+            return null;
+        }
+    }
+
+    private ApiResponse<Optional<ChipsModel>> hentVisningTest() {
+        try {
+            MvcResult mvcResult = this.mockMvc.perform(MockMvcRequestBuilders.get("/api/overblikkvisning").accept(MediaType.APPLICATION_JSON).contentType(MediaType.APPLICATION_JSON)).andReturn();
+
+            if (mvcResult.getResponse().getStatus() == HttpStatus.OK.value()) {
+                return new ApiResponse<>(mvcResult.getResponse().getStatus(), objectMapper.readValue(mvcResult.getResponse().getContentAsString(), new TypeReference<>() {
+                }), "");
+            } else {
+                return new ApiResponse<>(mvcResult.getResponse().getStatus(),
+                        null, mvcResult.getResolvedException().getMessage());
+            }
+        } catch (Exception e) {
+            Assertions.fail();
+            return null;
+        }
     }
 
 
