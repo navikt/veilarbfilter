3e88d2ec834b937c7a9113ba5fb011187b29dcce
Ingrid Fosså <ingrid.fossa@nav.no>
2025-01-08 16:12:05 +0100

    Opprettar enumar for Hovedmål (ny + gamal) slik at vi lettare kan mappe mellom dei

---
+++ src/main/java/no/nav/pto/veilarbfilter/domene/value/Hovedmal.java
@@ -0,0 +1,10 @@
+package no.nav.pto.veilarbfilter.domene.value;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public enum Hovedmal {
+    SKAFFE_ARBEID,
+    BEHOLDE_ARBEID,
+    OKE_DELTAKELSE;
+}
