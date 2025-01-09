31e6ee170f4ce00f2359858f6a24adc0a898defd
Ingrid Fosså <ingrid.fossa@nav.no>
2025-01-09 12:56:13 +0100

    Legg til testar for andre filtervalg og enumar for nye/gamle innsatsgrupper

--- src/test/java/no/nav/pto/veilarbfilter/repository/MineLagredeFilterRepositoryTest.java
+++ src/test/java/no/nav/pto/veilarbfilter/repository/MineLagredeFilterRepositoryTest.java
@@ -6,6 +6,9 @@ import no.nav.pto.veilarbfilter.domene.FilterModel;
 import no.nav.pto.veilarbfilter.domene.NyttFilterModel;
 import no.nav.pto.veilarbfilter.domene.PortefoljeFilter;
 import no.nav.pto.veilarbfilter.domene.value.ArenaHovedmal;
+import no.nav.pto.veilarbfilter.domene.value.ArenaInnsatsgruppe;
+import no.nav.pto.veilarbfilter.domene.value.Hovedmal;
+import no.nav.pto.veilarbfilter.domene.value.Innsatsgruppe;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -52,7 +55,7 @@ public class MineLagredeFilterRepositoryTest extends AbstractTest {
     }
 
     @Test
-    public void kanTelleAlleFilterMedGammeltHovedmalfilter() {
+    public void kanTelleAlleFilterMedGammeltHovedmalfiltervalg() {
         // Får rett når ingen filter er lagra
         Integer antallFilterForInserts = repository.tellMineFilterSomInneholderEnBestemtFiltertype(hovedmalfilterArena);
         Assertions.assertEquals(0, antallFilterForInserts);
@@ -76,5 +79,43 @@ public class MineLagredeFilterRepositoryTest extends AbstractTest {
 
         Integer antallFilterEtterInsertAvIkkehovedmalfilter = repository.tellMineFilterSomInneholderEnBestemtFiltertype(hovedmalfilterArena);
         Assertions.assertEquals(1, antallFilterEtterInsertAvIkkehovedmalfilter);
+    }
+
+    @Test
+    public void kanTelleFiltervalgForAlleHovedmalOgInnsatsgruppefiltervalg() {
+        // Given
+        String veilederId = "11223312345";
+
+        PortefoljeFilter filterMedAlleMigreringsfiltervalg = new PortefoljeFilter();
+        filterMedAlleMigreringsfiltervalg.setHovedmal(List.of(ArenaHovedmal.SKAFFEA.name()));
+        filterMedAlleMigreringsfiltervalg.setHovedmalGjeldendeVedtak14a(List.of(Hovedmal.SKAFFE_ARBEID.name()));
+        filterMedAlleMigreringsfiltervalg.setInnsatsgruppe(List.of(ArenaInnsatsgruppe.BATT.name()));
+        filterMedAlleMigreringsfiltervalg.setInnsatsgruppeGjeldendeVedtak14a(List.of(Innsatsgruppe.STANDARD_INNSATS.name()));
+
+        repository.lagreFilter(veilederId, new NyttFilterModel("filter", filterMedAlleMigreringsfiltervalg));
+
+        // When/Then
+        Assertions.assertEquals(1, repository.tellMineFilterSomInneholderEnBestemtFiltertype(hovedmalfilterArena));
+        Assertions.assertEquals(1, repository.tellMineFilterSomInneholderEnBestemtFiltertype(hovedmalfilterGjeldendeVedtak));
+        Assertions.assertEquals(1, repository.tellMineFilterSomInneholderEnBestemtFiltertype(innsatsgruppefilterArena));
+        Assertions.assertEquals(1, repository.tellMineFilterSomInneholderEnBestemtFiltertype(innsatsgruppefilterGjeldendeVedtak));
+    }
+
+    @Test
+    public void kanTelleFiltervalgNarDetErFilterForFlereBrukere() {
+        // Given
+        String veilederId1 = "01010111111";
+        String veilederId2 = "02020222222";
+        PortefoljeFilter filterMedHovedmal = new PortefoljeFilter();
+        filterMedHovedmal.setHovedmal(List.of(ArenaHovedmal.SKAFFEA.name()));
+
+        repository.lagreFilter(veilederId1, new NyttFilterModel("filter", filterMedHovedmal));
+        repository.lagreFilter(veilederId2, new NyttFilterModel("filter", filterMedHovedmal));
+
+        // When
+        Integer antallFilter = repository.tellMineFilterSomInneholderEnBestemtFiltertype(hovedmalfilterArena);
+
+        // Then
+        Assertions.assertEquals(2, antallFilter);
     }
 }
