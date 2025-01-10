a0f60cd1e3404235c2b2349db053a78d1bd088c3
Ingrid Fosså <ingrid.fossa@nav.no>
2025-01-10 14:30:51 +0100

    Gjer det mogleg å bytte frå arenahovedmål til hovedmålGjeldendeVedtak14a for eit og eit filter i eit "alt ligg til rette for det"-scenario

--- src/main/java/no/nav/pto/veilarbfilter/service/MineLagredeFilterService.java
+++ src/main/java/no/nav/pto/veilarbfilter/service/MineLagredeFilterService.java
@@ -1,13 +1,13 @@
 package no.nav.pto.veilarbfilter.service;
 
 import lombok.RequiredArgsConstructor;
-import no.nav.pto.veilarbfilter.domene.FilterModel;
-import no.nav.pto.veilarbfilter.domene.MineLagredeFilterModel;
-import no.nav.pto.veilarbfilter.domene.NyttFilterModel;
-import no.nav.pto.veilarbfilter.domene.SortOrder;
+import no.nav.pto.veilarbfilter.domene.*;
+import no.nav.pto.veilarbfilter.domene.value.ArenaHovedmal;
+import no.nav.pto.veilarbfilter.domene.value.Hovedmal;
 import no.nav.pto.veilarbfilter.repository.MineLagredeFilterRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
@@ -56,5 +56,33 @@ public class MineLagredeFilterService implements FilterService {
 
     public List<MineLagredeFilterModel> hentAllLagredeFilter() {
         return mineLagredeFilterRepository.hentAllLagredeFilter();
+    }
+
+
+    public void erstattArenahovedmalfiltervalgMedHovedmalGjeldendeVedtak14aFiltervalg(String veilederId, Integer filterId) {
+        try {
+            FilterModel filterSomSkalOppdateres = mineLagredeFilterRepository.hentFilter(filterId).orElseThrow(); // todo handter feil ved henting
+
+            // Lag liste over migrerte hovedmål
+            List<ArenaHovedmal> arenahovedmal = filterSomSkalOppdateres.getFilterValg().getHovedmal().stream().map(ArenaHovedmal::valueOf).toList();
+            List<Hovedmal> hovedmalGjeldendeVedtak = arenahovedmal.stream()
+                    .map(ArenaHovedmal::mapTilHovedmalGjeldendeVedtak14a)
+                    .toList();
+            List<String> hovedmalGjeldendeVedtakSomStreng = hovedmalGjeldendeVedtak.stream().map(it -> it.name()).toList();
+
+            // Todo slå saman nye og gamle HovedmalGjeldendeVedtak-filter
+
+            // Lag oppdatert porteføljefilter
+            PortefoljeFilter portefoljeFilterSomSkalOppdateres = filterSomSkalOppdateres.getFilterValg();
+            portefoljeFilterSomSkalOppdateres.setHovedmalGjeldendeVedtak14a(hovedmalGjeldendeVedtakSomStreng);
+            portefoljeFilterSomSkalOppdateres.setHovedmal(Collections.emptyList());
+
+            // Lag klart oppdatert filtermodell og skriv tilbake til databasen
+            filterSomSkalOppdateres.setFilterValg(portefoljeFilterSomSkalOppdateres);
+            mineLagredeFilterRepository.oppdaterFilter(veilederId, filterSomSkalOppdateres); // todo handter feil ved skriving
+        } catch (Exception e) {
+            // todo feilhåndtering her
+            throw e;
+        }
     }
 }
