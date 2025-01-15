50b226d3245122ab90ab3bdd76823c209a2a552c
Sondre Larsen Ovrid <sondre.ovrid@bekk.no>
2025-01-15 13:13:25 +0100

    Trekk ut migrering frå gamalt til nytt hovedmål i eigen metode

--- src/main/java/no/nav/pto/veilarbfilter/service/MigrerFilterService.java
+++ src/main/java/no/nav/pto/veilarbfilter/service/MigrerFilterService.java
@@ -8,7 +8,7 @@ import no.nav.pto.veilarbfilter.domene.PortefoljeFilter;
 import no.nav.pto.veilarbfilter.domene.value.ArenaHovedmal;
 import no.nav.pto.veilarbfilter.domene.value.Hovedmal;
 import no.nav.pto.veilarbfilter.repository.FilterRepository;
-import no.nav.pto.veilarbfilter.repository.MineLagredeFilterRepository;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Service;
 
 import java.util.Collections;
@@ -27,10 +27,32 @@ public class MigrerFilterService {
 
     public void migrerFilter(int batchStorrelse) {
         List<FilterModel> filtreSomSkalMigreres = filterRepository.hentMineFilterSomInneholderEnBestemtFiltertype(HOVEDMAL_FILTERVALG_JSON_KEY, batchStorrelse);
-
     }
 
     public void erstattArenahovedmalMedHovedmalGjeldendeVedtak14aIFiltervalg(FilterModel filterSomSkalOppdateres) throws JsonProcessingException {
+        // Lag liste over migrerte hovedmål
+        migrerArenaHovedmalTilGjeldendeVedtakHovedmalForFilter(filterSomSkalOppdateres);
+        filterRepository.oppdaterFilterValg(filterSomSkalOppdateres.getFilterId(), filterSomSkalOppdateres.getFilterValg()); // todo handter feil ved skriving
+    }
+
+    public void erstattArenahovedmalMedHovedmalGjeldendeVedtak14aIFiltervalgBatch(List<FilterModel> filtreSomSkalOppdateres) throws JsonProcessingException {
+        List<FilterRepository.FilterIdOgFilterValgPar> lagredeFilterMedMigrerteHovedmal = filtreSomSkalOppdateres.stream()
+                .peek(this::migrerArenaHovedmalTilGjeldendeVedtakHovedmalForFilter)
+                .map(this::mapTilFilterIdOgFilterValgPar)
+                .toList();
+
+        filterRepository.oppdaterFilterValgBatch(lagredeFilterMedMigrerteHovedmal);
+    }
+
+    private FilterRepository.FilterIdOgFilterValgPar mapTilFilterIdOgFilterValgPar(FilterModel mappetFilter) {
+        try {
+            return new FilterRepository.FilterIdOgFilterValgPar(mappetFilter.getFilterId(), objectMapper.writeValueAsString(mappetFilter.getFilterValg()));
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void migrerArenaHovedmalTilGjeldendeVedtakHovedmalForFilter(FilterModel filterSomSkalOppdateres) {
         // Lag liste over migrerte hovedmål
         List<String> hovedmalFraArenahovedmal = lagGjeldendeVedtakHovedmalFraArenahovedmal(filterSomSkalOppdateres.getFilterValg().getHovedmal());
 
@@ -48,41 +70,6 @@ public class MigrerFilterService {
 
         // Lag klart oppdatert filtermodell og skriv tilbake til databasen
         filterSomSkalOppdateres.setFilterValg(portefoljeFilterSomSkalOppdateres);
-        filterRepository.oppdaterFilterValg(filterSomSkalOppdateres.getFilterId(), filterSomSkalOppdateres.getFilterValg()); // todo handter feil ved skriving
-    }
-
-    public void erstattArenahovedmalMedHovedmalGjeldendeVedtak14aIFiltervalgBatch(List<FilterModel> filtreSomSkalOppdateres) throws JsonProcessingException {
-        List<FilterModel> mappedeFilter = filtreSomSkalOppdateres.stream().map(filterSomSkalOppdateres -> {
-        // Lag liste over migrerte hovedmål
-            List<String> hovedmalFraArenahovedmal = lagGjeldendeVedtakHovedmalFraArenahovedmal(filterSomSkalOppdateres.getFilterValg().getHovedmal());
-
-            // Slå saman nye og gamle HovedmalGjeldendeVedtak-filter
-            List<String> hovedmalFraGjeldendeVedtak14a = filterSomSkalOppdateres.getFilterValg().getHovedmalGjeldendeVedtak14a();
-            Set<String> alleUnikeHovedmal = new HashSet<>(hovedmalFraGjeldendeVedtak14a);
-            alleUnikeHovedmal.addAll(hovedmalFraArenahovedmal);
-
-            List<String> unikeSorterteHovedmal = alleUnikeHovedmal.stream().sorted().toList();
-
-            // Lag oppdatert porteføljefilter
-            PortefoljeFilter portefoljeFilterSomSkalOppdateres = filterSomSkalOppdateres.getFilterValg();
-            portefoljeFilterSomSkalOppdateres.setHovedmalGjeldendeVedtak14a(unikeSorterteHovedmal);
-            portefoljeFilterSomSkalOppdateres.setHovedmal(Collections.emptyList());
-
-            // Lag klart oppdatert filtermodell og skriv tilbake til databasen
-            filterSomSkalOppdateres.setFilterValg(portefoljeFilterSomSkalOppdateres);
-            return filterSomSkalOppdateres;
-        }).toList();
-
-        // TODO: Feilhåndtering TM - aka. skriv om til Kotlin
-        List<FilterRepository.FilterIdOgFilterValgPar> mappedeFilterPar = mappedeFilter.stream().map(mappetFilter -> {
-            try {
-                return new FilterRepository.FilterIdOgFilterValgPar(mappetFilter.getFilterId(), objectMapper.writeValueAsString(mappetFilter.getFilterValg()));
-            } catch (JsonProcessingException e) {
-                throw new RuntimeException(e);
-            }
-        }).toList();
-
-        filterRepository.oppdaterFilterValgBatch(mappedeFilterPar);
     }
 
     private List<String> lagGjeldendeVedtakHovedmalFraArenahovedmal(List<String> arenahovedmal) {
