976226bf43261e14adaa1d3048a140e6fd52d613
Ingrid Fosså <ingrid.fossa@nav.no>
2025-01-21 15:15:19 +0100

    Legg til enumar for nye og gamle registreringstyper

---
+++ src/main/java/no/nav/pto/veilarbfilter/domene/value/UtdaterteRegistreringstyper.java
@@ -0,0 +1,13 @@
+package no.nav.pto.veilarbfilter.domene.value;
+
+public enum UtdaterteRegistreringstyper {
+    MISTET_JOBBEN,
+    OPPSIGELSE,
+    JOBB_OVER_2_AAR,
+    VIL_FORTSETTE_I_JOBB,
+    INGEN_SVAR,
+    INGEN_VERDI,
+    ENDRET_PERMITTERINGSPROSENT,
+    TILBAKE_TIL_JOBB,
+    SAGT_OPP
+}
