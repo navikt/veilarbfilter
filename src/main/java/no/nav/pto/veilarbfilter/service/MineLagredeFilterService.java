9d07af10755c23f3bd5dfebe151ae49e6656aeb2
Kristina Skatun <kristinaskatun@gmail.com>
2025-10-17 11:38:50 +0200

    Fjern AAP-filter kode etter migrering er ferdig og toggel fjernet

--- src/main/java/no/nav/pto/veilarbfilter/service/MineLagredeFilterService.java
+++ src/main/java/no/nav/pto/veilarbfilter/service/MineLagredeFilterService.java
@@ -1,17 +1,11 @@
 package no.nav.pto.veilarbfilter.service;
 
-import io.getunleash.DefaultUnleash;
-import io.getunleash.UnleashContext;
 import lombok.RequiredArgsConstructor;
-import no.nav.pto.veilarbfilter.auth.AuthUtils;
-import no.nav.pto.veilarbfilter.config.FeatureToggle;
 import no.nav.pto.veilarbfilter.domene.*;
 import no.nav.pto.veilarbfilter.repository.MineLagredeFilterRepository;
 import org.springframework.stereotype.Service;
 
-import java.util.Collections;
 import java.util.List;
-import java.util.Objects;
 import java.util.Optional;
 
 
@@ -19,13 +13,10 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class MineLagredeFilterService implements FilterService {
     private final MineLagredeFilterRepository mineLagredeFilterRepository;
-    private final DefaultUnleash defaultUnleash;
 
     @Override
     public Optional<FilterModel> lagreFilter(String veilederId, NyttFilterModel nyttFilter) throws IllegalArgumentException {
         try {
-            PortefoljeFilter filtereMedKopiertAapArenaFilter = leggTilAapFraYtelseINyttAapArenaFilter(nyttFilter.getFilterValg());
-            nyttFilter.setFilterValg(filtereMedKopiertAapArenaFilter);
             return mineLagredeFilterRepository.lagreFilter(veilederId, nyttFilter);
         } catch (IllegalArgumentException e) {
             throw e;
@@ -35,8 +26,6 @@ public class MineLagredeFilterService implements FilterService {
     @Override
     public Optional<FilterModel> oppdaterFilter(String veilederId, FilterModel filter) throws IllegalArgumentException {
         try {
-            PortefoljeFilter filtereMedKopiertAapArenaFilter = leggTilAapFraYtelseINyttAapArenaFilter(filter.getFilterValg());
-            filter.setFilterValg(filtereMedKopiertAapArenaFilter);
             return mineLagredeFilterRepository.oppdaterFilter(veilederId, filter);
         } catch (IllegalArgumentException e) {
             throw e;
@@ -45,16 +34,12 @@ public class MineLagredeFilterService implements FilterService {
 
     @Override
     public Optional<FilterModel> hentFilter(Integer filterId) {
-        Optional<FilterModel> filterModel = mineLagredeFilterRepository.hentFilter(filterId);
-        return fjernDuplikatAvAapArenaFilterTilFrontend(filterModel);
+        return mineLagredeFilterRepository.hentFilter(filterId);
     }
 
     @Override
     public List<FilterModel> finnFilterForFilterBruker(String veilederId) {
-        List<FilterModel> filterModelListe = mineLagredeFilterRepository.finnFilterForFilterBruker(veilederId);
-        return filterModelListe.stream()
-                .map(this::fjernDuplikatAvAapArenaFilterTilFrontend)
-                .toList();
+        return mineLagredeFilterRepository.finnFilterForFilterBruker(veilederId);
     }
 
     @Override
@@ -71,56 +56,4 @@ public class MineLagredeFilterService implements FilterService {
         return mineLagredeFilterRepository.hentAllLagredeFilter();
     }
 
-    // I en mellomfase hvor vi har trukket ut arena filteret for aap til et nytt et må vi lagre dobbelt.
-    // I det vi har gått helt over til det nye filteret kan denne funksjonen slettes.
-    private PortefoljeFilter leggTilAapFraYtelseINyttAapArenaFilter(PortefoljeFilter filter) {
-        String aapYtelse = filter.getYtelse();
-        boolean aapArenaUnntak = Objects.equals(aapYtelse, "AAP_UNNTAK");
-        boolean aapArenaOrdinar = Objects.equals(aapYtelse, "AAP_MAXTID");
-        boolean aapArena = Objects.equals(aapYtelse, "AAP");
-
-        if (!aapArenaUnntak && !aapArenaOrdinar && !aapArena) {
-            return filter;
-        }
-
-        List<String> aapNyttFilterListe;
-
-        if (aapArenaUnntak) {
-            aapNyttFilterListe = List.of("HAR_AAP_UNNTAK");
-        } else if (aapArenaOrdinar) {
-            aapNyttFilterListe = List.of("HAR_AAP_ORDINAR");
-        } else {
-            aapNyttFilterListe = List.of("HAR_AAP_ORDINAR", "HAR_AAP_UNNTAK");
-        }
-
-        filter.setYtelseAapArena(aapNyttFilterListe);
-        return filter;
-    }
-
-    private Optional<FilterModel> fjernDuplikatAvAapArenaFilterTilFrontend(Optional<FilterModel> filterModelOptional) {
-        return filterModelOptional.map(this::fjernDuplikatAvAapArenaFilterTilFrontend);
-    }
-
-    // Når toggle er på skal kun det nye AAP filteret returneres. Når den er av skal kun det gamle returneres.
-    private FilterModel fjernDuplikatAvAapArenaFilterTilFrontend(FilterModel filter) {
-        String veilederId = AuthUtils.getInnloggetVeilederIdent().toString();
-        UnleashContext unleashContext = UnleashContext.builder()
-                .userId(veilederId)
-                .build();
-
-        boolean brukNyttAapFilterErSkruddPå = defaultUnleash.isEnabled(FeatureToggle.BRUK_NYTT_ARENA_AAP_FILTER, unleashContext);
-
-        if (brukNyttAapFilterErSkruddPå) {
-            String gammeltYtelseFilter = filter.getFilterValg().getYtelse();
-            if (gammeltYtelseFilter != null) {
-                List<String> muligeAapYtelseFiltre = List.of("AAP", "AAP_MAXTID", "AAP_UNNTAK");
-                if (muligeAapYtelseFiltre.contains(gammeltYtelseFilter)) {
-                    filter.getFilterValg().setYtelse(null);
-                }
-            }
-        } else {
-            filter.getFilterValg().setYtelseAapArena(Collections.emptyList());
-        }
-        return filter;
-    }
 }
