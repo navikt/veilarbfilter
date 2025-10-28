8462495e610b165a99bc3f1433cd4ddea19a132a
Ingrid Fosså <ingrid.fossa@nav.no>
2025-10-28 13:48:07 +0100

    Filtrer bort lagra filter som inneheld avviks-filteret.

--- src/main/java/no/nav/pto/veilarbfilter/service/MineLagredeFilterService.java
+++ src/main/java/no/nav/pto/veilarbfilter/service/MineLagredeFilterService.java
@@ -1,17 +1,24 @@
 package no.nav.pto.veilarbfilter.service;
 
+import io.getunleash.DefaultUnleash;
+import io.getunleash.UnleashContext;
 import lombok.RequiredArgsConstructor;
+import no.nav.pto.veilarbfilter.auth.AuthUtils;
+import no.nav.pto.veilarbfilter.config.FeatureToggle;
+import no.nav.pto.veilarbfilter.database.Table;
 import no.nav.pto.veilarbfilter.domene.*;
 import no.nav.pto.veilarbfilter.repository.MineLagredeFilterRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class MineLagredeFilterService implements FilterService {
     private final MineLagredeFilterRepository mineLagredeFilterRepository;
+    private final DefaultUnleash defaultUnleash;
 
     @Override
     public Optional<FilterModel> lagreFilter(String veilederId, NyttFilterModel nyttFilter) throws IllegalArgumentException {
@@ -33,12 +40,14 @@ public class MineLagredeFilterService implements FilterService {
 
     @Override
     public Optional<FilterModel> hentFilter(Integer filterId) {
-        return mineLagredeFilterRepository.hentFilter(filterId);
+        Optional<FilterModel> filterModel = mineLagredeFilterRepository.hentFilter(filterId);
+        return filtrerUtAvvik14aFilterTilFrontend(filterModel);
     }
 
     @Override
     public List<FilterModel> finnFilterForFilterBruker(String veilederId) {
-        return mineLagredeFilterRepository.finnFilterForFilterBruker(veilederId);
+        List<FilterModel> filterModelListe = mineLagredeFilterRepository.finnFilterForFilterBruker(veilederId);
+        return filterModelListe.stream().map(this::filtrerUtAvvik14aFilterTilFrontend).filter(Objects::nonNull).toList();
     }
 
     @Override
@@ -53,5 +62,34 @@ public class MineLagredeFilterService implements FilterService {
 
     public List<MineLagredeFilterModel> hentAllLagredeFilter() {
         return mineLagredeFilterRepository.hentAllLagredeFilter();
+    }
+
+    private Optional<FilterModel> filtrerUtAvvik14aFilterTilFrontend(Optional<FilterModel> filterModelOptional) {
+        return filterModelOptional.map(this::filtrerUtAvvik14aFilterTilFrontend);
+    }
+
+    /* I ein mellomfase der vi har skrudd på feature-toggle for å skjule avvik-filteret
+     * Filtrerar vi vekk filter som inneheld det. Vi har på førehand sjekka at omfanget er lite, og at filtera berre handlar om avvik mellom gjeldande vedtak og Arena. */
+    private FilterModel filtrerUtAvvik14aFilterTilFrontend(FilterModel filter) {
+        String veilederId = AuthUtils.getInnloggetVeilederIdent().toString();
+        UnleashContext unleashContext = UnleashContext.builder()
+                .userId(veilederId)
+                .build();
+
+        boolean skjulFilterSammenligneGjeldende14aOgArena = defaultUnleash.isEnabled(FeatureToggle.SKJUL_FILTER_SAMMENLIGNE_GJELDENDE_14A_OG_ARENA, unleashContext);
+
+        if (!skjulFilterSammenligneGjeldende14aOgArena) {
+            return filter;
+        } else {
+            List<String> avvik14aFilter = filter.getFilterValg().getAvvik14aVedtak();
+
+            // Om filteret ikkje inneheld avviksfilter: returner det.
+            if (avvik14aFilter.isEmpty()) {
+                return filter;
+            }
+
+            // Om det inneheld avviksfilter: filtrer det bort.
+            return null;
+        }
     }
 }
