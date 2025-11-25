5a57333399266f443b79466597f0c9f8d768293e
Klara Margrethe Helgemo <klara.margrethe.helgemo@nav.no>
2025-11-25 14:21:56 +0100

    Bytter ut SecureLog med Team log etter mønster fra veilarbportefolje

--- src/main/java/no/nav/pto/veilarbfilter/util/SecureLogUtils.kt
+++ src/main/java/no/nav/pto/veilarbfilter/util/SecureLogUtils.kt
@@ -1,12 +1,36 @@
-package no.nav.pto.veilarbfilter.util;
+package no.nav.pto.veilarbfilter.util
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.slf4j.LoggerFactory
+import org.slf4j.MarkerFactory
 
-public final class SecureLogUtils {
-    private SecureLogUtils() {
-        throw new UnsupportedOperationException();
+object SecureLogUtils {
+    @JvmField
+    val secureLog = TeamLogsWrapper()
+}
+
+class TeamLogsWrapper {
+    val marker = MarkerFactory.getMarker("TEAM_LOGS")
+    val teamLog = LoggerFactory.getLogger("team-logs-logger")
+
+    fun debug(msg: String, vararg args: Any) = teamLog.info(marker, msg, *args)
+
+    fun info(msg: String, vararg args: Any) = teamLog.info(marker, msg, *args)
+
+    @JvmOverloads
+    fun warn(msg: String, error: Throwable? = null, vararg args: Any) {
+        if (error != null) {
+            teamLog.warn(marker, msg, error)
+        } else {
+            teamLog.warn(marker, msg, *args)
+        }
     }
 
-    public static final Logger secureLog = LoggerFactory.getLogger("SecureLog");
+    @JvmOverloads
+    fun error(msg: String, error: Throwable? = null, vararg args: Any) {
+        if (error != null) {
+            teamLog.error(marker, msg, error)
+        } else {
+            teamLog.error(marker, msg, *args)
+        }
+    }
 }
